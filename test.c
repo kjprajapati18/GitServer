7c63dd47d057040243291f3ee743ef2314ee6e3f
Rakshay Kanthadai <rsk146@ilab3.cs.rutgers.edu>
2020-05-05 01:03:39 -0400

    test nees to be tested

--- test.c
+++ test.c
@@ -1,5 +1,43 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]){
-    
+    int pid = fork();
+    if(pid == 0){
+        int fd = open("serverStdOut.txt", O_CREAT|O_WRONLY, 00600);
+        dup2(fd ,1);
+        close(fd);
+        //call exec
+        char* argv[2] = {"9998", NULL};
+        execv("./server/WTFserver", argv);
+        //system("cd server; ./WTFserver 9999 > ../serverStdOut.txt &");
+    }
+    else{
+        system("cd client; ./WTF configure localhost 9998 > ../clientStdOut.txt");
+        system("cd client; ./WTF create testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF create destroyThis >> ../clientStdOut.txt");
+        system("cd client; ./WTF destroy destroyThis >> ../clientStdOut.txt");
+        system("cd client; touch testProject/test.txt; echo \"abcde\" > testProject/test.txt");
+        system("cd client; touch testProject/removeThis.txt");
+        system("cd client; ./WTF add testProject test.txt >> ../clientStdOut.txt");
+        system("cd client; ./WTF add testProject removeThis.txt >> ../clientStdOut.txt");
+        system("cd client; ./WTF commit testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF push testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF remove testProject removeThis.txt >> ../clientStdOut.txt");
+        system("cd client; ./WTF commit testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF push testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF rollback testProject 1 >> ../clientStdOut.txt");
+        system("cd client; ./WTF update testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF upgrade testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF currentversion testProject >> ../clientStdOut.txt");
+        system("cd client; ./WTF history testProject >> ../clientStdOut.txt");
+        system("cd client; rm -r testProject");
+        system("cd client; ./WTF checkout testProject >> ../clientStdOut.txt");
+        char buffer[20]; 
+        sprintf(buffer, "kill %s", pid);
+        system(buffer);
+        wait();
+    }*/
 }
